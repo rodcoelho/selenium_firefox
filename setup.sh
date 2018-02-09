6837d3d7dc42b6801780174f07caca0259786277
rodcoelho <rodcpython@gmail.com>
2018-02-09 12:49:26 -0500

    setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,10 @@ sudo apt-get install python-pip
 
 sudo pip install selenium
 
+wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz
+
+tar -xvzf geckodriver-v0.18.0-linux64.tar.gz
+
+chmod +x geckodriver
+
+export PATH=$PATH:/usr/bin/env/geckodriver
