c0f571c723b9a2db5ddcf3fbd93939d138795210
rodcoelho <rodcpython@gmail.com>
2018-02-09 12:51:34 -0500

    firefox path geckodriver fixed?

--- firefox_selenium_script.py
+++ firefox_selenium_script.py
@@ -25,7 +25,7 @@ dates = [
 payload = {}
 
 def init_driver():
-    driver = webdriver.Firefox(executable_path='/usr/bin/env/')
+    driver = webdriver.Firefox(executable_path='/usr/bin/env/geckodriver')
     driver.wait = WebDriverWait(driver, 5)
     return driver
 
